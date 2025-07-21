f72da02b2261972962a8db07226ce6fc5bc03ebe
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 11:05:35 +0300

    feat: add Utilities component with flexible styling for improved layout

---
+++ src/components/Utilities.jsx
@@ -0,0 +1,11 @@
+const utilitiesStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "1.25rem",
+};
+
+function Utilities() {
+  return <div style={utilitiesStyle}></div>;
+}
+
+export default Utilities;
