d341325e924c3b6e9f13b548998d6da111b7f49b
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 11:02:30 +0300

    feat: restructure Dashboard component to include NavBar and Logo for improved layout

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,13 @@
+import Logo from "../../components/Logo";
+import NavBar from "../../components/navBar";
 import styles from "./Dashboard.module.css";
 
 function Dashboard() {
   return (
     <main className={styles.dashboard}>
-      <h1>Dashboard</h1>
+      <NavBar>
+        <Logo />
+      </NavBar>
     </main>
   );
 }
