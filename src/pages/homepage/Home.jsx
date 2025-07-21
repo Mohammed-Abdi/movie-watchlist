c9d33991a7d9fdb3ea1a7a6f3fc45fb5329ace38
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 09:10:20 +0300

    feat: update Home component to include NavBar with logo and explore button

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,7 +1,15 @@
+import NavBar from "../../components/navBar";
 import "./Home.module.css";
 
 function Home() {
-  return <main>Homepage</main>;
+  return (
+    <main>
+      <NavBar>
+        <div>Logo</div>
+        <button>Explore</button>
+      </NavBar>
+    </main>
+  );
 }
 
 export default Home;
