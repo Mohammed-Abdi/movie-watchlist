94e8af2b79bc1eab6fbb9311edf4ecc890509cb9
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 12:30:58 +0300

    feat: enhance Logo component to accept dynamic text and update Watchlist page layout

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const logoStyle = { fontSize: "1.5rem", fontWeight: 500 };
 
-function Logo() {
-  return <h1 style={logoStyle}>WatchList</h1>;
+function Logo({ text }) {
+  return <h1 style={logoStyle}>{text || "WatchList"}</h1>;
 }
 
 export default Logo;
--- src/pages/watchlist/Watchlist.jsx
+++ src/pages/watchlist/Watchlist.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import Back from "../../assets/Back";
+import NavBar from "../../components/navBar";
+import SecondaryButton from "../../components/SecondaryButton";
 import styles from "./Watchlist.module.css";
+import Logo from "../../components/Logo";
 
 function Watchlist() {
-  return <main className={styles.Watchlist}>Watchlist</main>;
+  return (
+    <main className={styles.Watchlist}>
+      <NavBar
+        style={{
+          justifyContent: "none",
+          gap: "0.625rem",
+        }}
+      >
+        <Link to="/dashboard" style={{ color: "inherit" }}>
+          <SecondaryButton>
+            <Back style={{ opacity: 0.8 }} />
+          </SecondaryButton>
+        </Link>
+        <Logo text="Your WatchList" />
+      </NavBar>
+    </main>
+  );
 }
 
 export default Watchlist;
