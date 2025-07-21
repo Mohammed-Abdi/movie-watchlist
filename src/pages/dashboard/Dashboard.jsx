fdf27e3e2a9dc739bbe817ec6b668b1c78a64c81
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 11:00:46 +0300

    feat: add Dashboard component and routing for improved navigation

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import styles from "./Dashboard.module.css";
+
+function Dashboard() {
+  return (
+    <main className={styles.dashboard}>
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
+
+export default Dashboard;
