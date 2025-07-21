926fa99ddb57dc3b085c2530013788632b4b9822
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 11:28:59 +0300

    feat: implement SecondaryButton component for consistent button styling in Utilities

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -1,19 +1,28 @@
 import Bell from "../assets/Bell";
 import Bookmark from "../assets/Bookmark";
 import Search from "../assets/Search";
+import SecondaryButton from "./SecondaryButton";
 
 const utilitiesStyle = {
   display: "flex",
   alignItems: "center",
-  gap: "1.25rem",
+  gap: "0.625rem",
 };
 
 function Utilities() {
   return (
     <div style={utilitiesStyle}>
-      <Search />
-      <Bookmark />
-      <Bell />
+      <SecondaryButton>
+        <Search style={{ opacity: 0.8 }} />
+      </SecondaryButton>
+
+      <SecondaryButton>
+        <Bookmark style={{ opacity: 0.8 }} />
+      </SecondaryButton>
+
+      <SecondaryButton>
+        <Bell style={{ opacity: 0.8 }} />
+      </SecondaryButton>
     </div>
   );
 }
