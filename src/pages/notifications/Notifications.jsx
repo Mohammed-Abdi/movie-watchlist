841034f7354eded52ad5429cdde92eb48c7e41ac
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-21 12:32:27 +0300

    feat: implement Notifications page layout with NavBar and Back button

--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+import NavBar from "../../components/navBar";
 import styles from "./Notifications.module.css";
+import SecondaryButton from "../../components/SecondaryButton";
+import Back from "../../assets/Back";
+import Logo from "../../components/Logo";
 
 function Notifications() {
-  return <main className={styles.Notifications}>Notifications</main>;
+  return (
+    <main className={styles.Notifications}>
+      {" "}
+      <NavBar
+        style={{
+          justifyContent: "none",
+          gap: "0.625rem",
+        }}
+      >
+        <Link to="/dashboard" style={{ color: "inherit" }}>
+          <SecondaryButton>
+            <Back style={{ opacity: 0.8 }} />
+          </SecondaryButton>
+        </Link>
+        <Logo text="Notifications" />
+      </NavBar>
+    </main>
+  );
 }
 
 export default Notifications;
