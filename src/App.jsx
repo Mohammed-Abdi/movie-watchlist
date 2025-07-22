fe033a74fb7db2d8ea3915adc20f17f5e23b3c4b
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-22 16:42:18 +0300

    feat: wrap context to provide access to selected movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { useMovies } from "./hooks/useMovies";
+import { MovieContext } from "./context/MovieContext";
 
 function App() {
+  const { movie, dispatch } = useMovies();
+
   return (
     <BrowserRouter>
-      <Router />
+      <MovieContext.Provider value={{ movie, dispatch }}>
+        <Router />
+      </MovieContext.Provider>
     </BrowserRouter>
   );
 }
