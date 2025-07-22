bf674c2d145cf1f14b8a4db9f88d63ac5fa5f319
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-22 15:14:24 +0300

    feat: enhance movie card to support TV shows with different object structure

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,18 +10,22 @@ function Card({ movie }) {
           style={{ color: "#E7BF36", width: "16px", height: "16px" }}
           className={styles.starIcon}
         />
-        {movie.vote_average.toFixed(1)}
+        {movie?.vote_average.toFixed(1)}
       </div>
       <div className={styles.detail}>
-        <p style={{ fontWeight: 500 }}>{movie.title}</p>
+        <p style={{ fontWeight: 500 }}>
+          {movie?.title ? movie?.title : movie.original_name}
+        </p>
         <p style={{ fontSize: "0.75rem", opacity: 0.7 }}>
-          {movie.release_date.split("-").at(0)}
+          {movie?.release_date
+            ? movie?.release_date?.split("-").at(0)
+            : movie?.first_air_date?.split("-").at(0)}
         </p>
       </div>
       <img
         className={styles.poster}
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        alt={`${movie.title} poster`}
+        src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+        alt={`${movie?.title} poster`}
         width={150}
       />
     </Link>
