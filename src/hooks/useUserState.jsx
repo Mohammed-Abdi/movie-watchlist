90864e411801a41939eee7d5d4d51b2bc7656c3f
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-23 10:39:23 +0300

    feat: enhance reducer to add/remove movies from watchlist and update notifications

--- src/hooks/useUserState.jsx
+++ src/hooks/useUserState.jsx
@@ -8,8 +8,22 @@ const initialUserState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "something":
-      return "something";
+    case "ADD_MOVIE_TO_WATCHLIST":
+      return {
+        ...state,
+        watchList: [...state.watchList, action.payload.movie],
+        notifications: [...state.notifications, action.payload.notification],
+      };
+    case "REMOVE_MOVIE_FROM_WATCHLIST":
+      return {
+        ...state,
+        watchList: state.watchList.filter(
+          (movie) => movie.id === action.payload.id
+        ),
+        notifications: [...state.notifications, action.payload.notification],
+      };
+    case "CLEAR_NOTIFICATIONS":
+      return { ...state, newNotificationCount: 0 };
     default:
       throw new Error("Unknown action");
   }
