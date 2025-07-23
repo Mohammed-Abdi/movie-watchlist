93b710418d7f82c2733e4181c1f24226f080de71
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-23 22:06:37 +0300

    chore: change app name from Watchlist to WatchNest

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const logoStyle = { fontSize: "1.5rem", fontWeight: 500 };
 
 function Logo({ text }) {
-  return <h1 style={logoStyle}>{text || "WatchList"}</h1>;
+  return <h1 style={logoStyle}>{text || "WatchNest"}</h1>;
 }
 
 export default Logo;
