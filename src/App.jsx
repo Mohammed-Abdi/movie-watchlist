c0cf7c3fb71b1e203d4eb44b0babbc86572bd271
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-23 11:00:54 +0300

    feat: wrap app with SearchContext provider for global access

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
 import { useMovies } from "./hooks/useMovies";
 import { MovieContext } from "./context/MovieContext";
+import { useSearch } from "./hooks/useSearch";
+import { SearchContext } from "./context/SearchContext";
 
 function App() {
   const { movie, dispatch } = useMovies();
+  const { search, searchDispatch } = useSearch();
 
   return (
     <BrowserRouter>
-      <MovieContext.Provider value={{ movie, dispatch }}>
-        <Router />
-      </MovieContext.Provider>
+      <SearchContext.Provider value={{ search, searchDispatch }}>
+        <MovieContext.Provider value={{ movie, dispatch }}>
+          <Router />
+        </MovieContext.Provider>
+      </SearchContext.Provider>
     </BrowserRouter>
   );
 }
