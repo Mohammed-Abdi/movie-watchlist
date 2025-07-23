c1577fafaae2a318ef92632ded005bce6bb496d6
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-23 11:37:24 +0300

    feat: wrap app with UserContext provider for user state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import { useMovies } from "./hooks/useMovies";
 import { MovieContext } from "./context/MovieContext";
 import { useSearch } from "./hooks/useSearch";
 import { SearchContext } from "./context/SearchContext";
+import { UserContext } from "./context/UserContext";
+import { useUserState } from "./hooks/useUserState";
 
 function App() {
   const { movie, dispatch } = useMovies();
   const { search, searchDispatch } = useSearch();
+  const { user, userDispatch } = useUserState();
 
   return (
     <BrowserRouter>
-      <SearchContext.Provider value={{ search, searchDispatch }}>
-        <MovieContext.Provider value={{ movie, dispatch }}>
-          <Router />
-        </MovieContext.Provider>
-      </SearchContext.Provider>
+      <UserContext.Provider value={{ user, userDispatch }}>
+        <SearchContext.Provider value={{ search, searchDispatch }}>
+          <MovieContext.Provider value={{ movie, dispatch }}>
+            <Router />
+          </MovieContext.Provider>
+        </SearchContext.Provider>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
