ce49d85d04e85bab1887171011eda2122c44fe4c
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-25 06:36:41 +0300

    refactor: simplify Card component by removing isTvShow prop and redundant spread

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,19 +4,15 @@ import styles from "./Card.module.css";
 import { useContext } from "react";
 import { MovieContext } from "../../context/MovieContext";
 
-function Card({ movie, isTvShow }) {
+function Card({ movie }) {
   const { dispatch } = useContext(MovieContext);
-
-  const selectedMovie = { ...movie, isTvShow: isTvShow ? true : false };
 
   return (
     <Link
       to={`/movies-explore/movie/${movie.id}`}
       className={styles.cardWrapper}
       style={{ color: "inherit" }}
-      onClick={() =>
-        dispatch({ type: "SET_CURRENT_MOVIE", payload: selectedMovie })
-      }
+      onClick={() => dispatch({ type: "SET_CURRENT_MOVIE", payload: movie })}
     >
       <div className={styles.ratingBadge}>
         <Star
